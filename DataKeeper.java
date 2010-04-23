a6d053562852234e4c19c8593a84049c7977ba3f
HenChao <HenChao@5b529f88-db73-2024-aff8-1fe53d6144e5>
2010-04-23 19:39:49 +0000

    Added javadoc comments to Bill, DataKeeper, and Order classes

--- DataKeeper.java
+++ DataKeeper.java
@@ -1,7 +1,14 @@
+/**
+ * The DataKeeper class contains all the functions to be called from the other subsystems.
+ * @author henry
+ *
+ */
 
 public class DataKeeper {
 	
+	/**
+	 * Contains the global kitchen queue to be accessed by the chefs.
+	 */
 	static Kitchen globalKitchen;
-	static RecordKeeper recordKeeper;
 
 }
--- Order.java
+++ Order.java
@@ -1,25 +1,88 @@
+/**
+ * This class contains all the information regarding specific food orders.
+ * 
+ * @author Henry
+ *
+ */
 
 public class Order {
+	
+	/**
+	 * Name of the order
+	 */
 	String foodName;
+	
+	/**
+	 * The value of the food
+	 */
 	double price;
-	boolean status;	//when status == 1, order is ready
+	
+	/**
+	 * By default, will set status to 0, indicating the order is not ready.
+	 * Once the cook makes the order and is ready to give to the customer, the status
+	 * is set to 1.
+	 */
+	
+	boolean status;
+	
+	/**
+	 * Any comments regarding the dish, such as allergies or specific
+	 * ingredients will be kept here.
+	 */
 	String comments;
 	
-	Order(String name, String com){
+	/**
+	 * A reference to the bill to which it belongs
+	 */
+	Bill itsBill;
+	
+	/**
+	 * Default constructor of the class
+	 * 
+	 * By default, will set the status of the dish to not ready, or 0
+	 * @param name The name of the dish
+	 * @param com The comments regarding the order
+	 */
+		
+	Order(String name, String com, Bill b){
 		foodName = name;
 		comments = com;
+		itsBill = b;
 		status = false;
 		//Need to add some database queries here to set price
 		price = 0.00;
 	}
 	
+	/**
+	 * Gets the name of the order
+	 * @return A string containing the food name
+	 */
 	String getFoodName(){return foodName;}
 	
+	/**
+	 * Gets the price of the order
+	 * @return The price of the order as a double
+	 */
 	double getPrice(){return price;}
 	
+	/**
+	 * Gets the comment associated with the order
+	 * @return A string containing the comment
+	 */
 	String getComment(){return comments;}
 	
+	/**
+	 * Sets the status of the order
+	 * @param i A boolean value, with true indicating the order is done, 
+	 * and false indicating the order is not done 
+	 */
 	void setStatus(boolean i){status = i;}
+	
+	/**
+	 * Gets the status of the order
+	 * @return A boolean value, with true indicating the order is done,
+	 * and false indicating the order is not done
+	 */
 	boolean getStatus(){return status;}
 
 }
