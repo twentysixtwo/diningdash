1c348e5c18d23ee87f23aedafb2cc97a1a95bdc7
HenChao <HenChao@5b529f88-db73-2024-aff8-1fe53d6144e5>
2010-04-24 00:52:22 +0000

    Uploaded Javadocs for Data subsystem and updated data classes

--- DataKeeper.java
+++ DataKeeper.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /**
  * The DataKeeper class contains all the functions to be called from the other subsystems.
  * @author henry
@@ -9,6 +11,29 @@ public class DataKeeper {
 	/**
 	 * Contains the global kitchen queue to be accessed by the chefs.
 	 */
-	static Kitchen globalKitchen;
+	private static Kitchen globalKitchen;
+	
+	/**
+	 * Contains the map of the restaurant.
+	 */
+	private static Map restaurantMap;
+	
+	/**
+	 * Default constructor for DataKeeper. Will instantiate a new kitchen and map object.
+	 */
+	public DataKeeper(){
+		globalKitchen = new Kitchen();
+		restaurantMap = new Map();
+	}
+	
+	/**
+	 * Generates a linked list of tables to return.
+	 * @return A list of tables as a linked list.
+	 */
+	public LinkedList getTables(){
+		LinkedList<Table> listOfTables = new LinkedList<Table>();
+		//Fill out more stuff here
+		return listOfTables;
+	}
 
 }
