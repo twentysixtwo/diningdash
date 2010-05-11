6732ac73d80669437f7991848122251a9739b2ff
HenChao <HenChao@5b529f88-db73-2024-aff8-1fe53d6144e5>
2010-05-11 15:39:29 +0000

    Changed UI to call RecordKeeper to get the map data from the database

--- trunk/Map.java
+++ trunk/Map.java
@@ -22,21 +22,7 @@ public class Map {
 	 * <Table number, max number of people allowed at the table, waiter name, row position, col position>
 	 */
 	private void addTables(){
-		Table t1 = new Table(1, 4, null, 1, 1);
-		t1.setStatus(Color.red);
-			resturantTables.add(t1);
-		Table t2 = new Table(2, 4, null, 1, 3);
-			resturantTables.add(t2);
-		Table t3 = new Table(3, 4, null, 3, 1);
-		t3.setStatus(Color.yellow);
-			resturantTables.add(t3);
-		Table t4 = new Table(4, 4, null, 3, 3);
-			resturantTables.add(t4);
-		Table t5 = new Table(5, 4, null, 5, 1);
-			resturantTables.add(t5);
-		Table t6 = new Table(6, 4, null, 5, 3);
-		t6.setStatus(Color.red);
-			resturantTables.add(t6);
+		resturantTables = RecordKeeper.getMap();
 	}
 	
 	public int getTotalRows(){
